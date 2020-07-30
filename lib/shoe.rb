d1f53dccb4c06405ca5ec3f268f29edca433f44d
Steve Cordova <cordovas@bc.edu>
2020-07-30 20:46:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,8 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+
   end
 
   def cobble
@@ -13,8 +15,4 @@ BRANDS = []
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(*brand)
-    @brand = brand
-    BRANDS << brand
-end
 end
